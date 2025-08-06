0839d35152b91c41ef442450ed05188505254e52
gkalian <greatkalian@gmail.com>
2025-08-06 12:43:51 +0300

    switch to vite from webpack

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
+
+export default defineConfig({
+  plugins: [react()],
+  publicDir: 'public',
+  server: {
+    port: 3100,
+    open: true,
+    host: true,
+  },
+  base: '/',
+  build: {
+    outDir: 'build',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][ext]',
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+    assetsInlineLimit: 0,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  css: {
+    devSourcemap: true,
+  }
+});
